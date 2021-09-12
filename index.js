a81489d28c078f4072279367efd5cf02fa34ae2b
Ezequiel <martinez.ezequiel42@gmail.com>
2021-09-12 04:21:34 -0700

    required inquirer and added framework for input promps

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const inquirer = require('inquirer')
 
 class Employee {
 	constructor (name, id, email){
@@ -36,3 +37,28 @@ class Employee {
 		super(name, id, email)}
 		getOfficeNumber() {this.officeNumber}
 }
+
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your user name?',
+      name: 'name',
+    },
+    {
+      type: 'input',
+      message: 'What is your password?',
+      name: 'id',
+    },
+    {
+      type: 'input',
+      message: 'Re-enter password to confirm:',
+      name: 'email',
+    },
+  ])
+  .then((response) =>
+    response.confirm === response.password
+      ? console.log('Success!')
+      : console.log('You forgot your password already '+response.username+'?!')
+  );
