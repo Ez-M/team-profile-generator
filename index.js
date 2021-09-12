8922a4c6f5a526e86c8050bbdefb46748638008b
Ezequiel <martinez.ezequiel42@gmail.com>
2021-09-12 04:20:49 -0700

    added additional subclasses

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-
 class Employee {
 	constructor (name, id, email){
 	this.name = name;
@@ -11,4 +10,29 @@ class Employee {
 	getId ()   {return this.id}
 	getEmail() {return this.email}
 	getRole()  {return this}
+}
+
+
+	class Engineer extends Employee {
+		constructor (github) {
+		this.github = github;
+
+		super(this.name, id, email)}
+		getGithub() {return this.github}
+
+}
+
+	class Intern extends Employee {
+		constructor (school) {
+		this.school = data.school 
+
+		super(name, id, email)}
+		getSchool() {this.school}
+}
+	class Manager extends Employee {
+		constructor (officeNumber){
+		this.officeNumber = data.officeNumber
+
+		super(name, id, email)}
+		getOfficeNumber() {this.officeNumber}
 }
