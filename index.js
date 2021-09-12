4f803a25a0c03e37697106dd0a235ed9b9232a57
Ezequiel <martinez.ezequiel42@gmail.com>
2021-09-12 14:24:57 -0700

    added a placeholder variable named empHolder and assigned input values into the empHolder whenever primary questions are asked

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 const inquirer = require('inquirer');
 const roles = require('./roles');
 
+var empHolder = {
+    name: "",
+    id: "",
+    email: "",
+    github: "",
+    school: "",
+    officeNumber: "",
+}
 
 
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: 'What is your user name?',
-      name: 'name',
-    },
-    {
-      type: 'input',
-      message: 'What is your password?',
-      name: 'id',
-    },
-    {
-      type: 'input',
-      message: 'Re-enter password to confirm:',
-      name: 'email',
-    },
-  ])
-  .then((response) =>
-    response.confirm === response.password
-      ? console.log('Success!')
-      : console.log('You forgot your password already '+response.username+'?!')
-  );
+function nameIdEmail() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is your user name?',
+                name: 'name',
+            },
+            {
+                type: 'input',
+                message: 'What is your password?',
+                name: 'id',
+            },
+            {
+                type: 'input',
+                message: 'Re-enter password to confirm:',
+                name: 'email',
+            },
+        ])
+        .then((response) => {
+            empHolder.name = response.name;
+            empHolder.id = response.id;
+            empHolder.email = response.email;
+
+        }
+
+        );
+}
--- roles.js
+++ roles.js
@@ -1,37 +1,40 @@
 class Employee {
-	constructor (name, id, email){
-	this.name = name;
-	this.id = id;
-	this.email = email;	
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName() { return this.name }
+    getId() { return this.id }
+    getEmail() { return this.email }
+    getRole() { return this }
 }
 
-	getName()  {return this.name}
-	getId ()   {return this.id}
-	getEmail() {return this.email}
-	getRole()  {return this}
-}
 
+class Engineer extends Employee {
+    constructor(github) {
+        this.github = github;
 
-	class Engineer extends Employee {
-		constructor (github) {
-		this.github = github;
-
-		super(this.name, id, email)}
-		getGithub() {return this.github}
+        super(name, id, email)
+    }
+    getGithub() { return this.github }
 
 }
 
-	class Intern extends Employee {
-		constructor (school) {
-		this.school = data.school 
+class Intern extends Employee {
+    constructor(school) {
+        this.school = school
 
-		super(name, id, email)}
-		getSchool() {this.school}
+        super(name, id, email)
+    }
+    getSchool() { this.school }
 }
-	class Manager extends Employee {
-		constructor (officeNumber){
-		this.officeNumber = data.officeNumber
+class Manager extends Employee {
+    constructor(officeNumber) {
+        this.officeNumber = officeNumber
 
-		super(name, id, email)}
-		getOfficeNumber() {this.officeNumber}
+        super(name, id, email)
+    }
+    getOfficeNumber() { this.officeNumber }
 }
